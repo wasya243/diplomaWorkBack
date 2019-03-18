d7b7a1e83b0e14a59681c57157b0fe0083b5bc18
Vasyl Kharhenko <v.kharchenko@mozidev.com>
2019-03-18 17:25:00 +0200

    Setup connection to postgre & create initial routes for users.

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ const bodyParser = require('body-parser');
 
 dotenv.config({path: path.resolve(__dirname, '.env')});
 
+const db = require('./queries');
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-	res.json({info: 'Node.js, Express, and Postgres API'});
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
 });
 
-const {PORT} = process.env;
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
 
-app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+const {SERVER_PORT} = process.env;
+
+app.listen(SERVER_PORT, () => console.log(`Server is listening on ${SERVER_PORT}`));
