2e86cd20c4aa8b7a1280c321965e8fea6bb27fdb
Vasyl Kharhenko <v.kharchenko@mozidev.com>
2019-03-18 17:00:45 +0200

    Start using bodyParser & add first route.

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 
 dotenv.config({path: path.resolve(__dirname, '.env')});
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+	res.json({info: 'Node.js, Express, and Postgres API'});
+});
 
 const {PORT} = process.env;
 
