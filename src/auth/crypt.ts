b8cd96503277a2db2281ab4dcd1b66fb71be2f63
Vasyl Kharhenko <v.kharchenko@mozidev.com>
2019-03-21 14:08:05 +0200

    Fix return type for encryptPassword funciton.

--- src/auth/crypt.ts
+++ src/auth/crypt.ts
@@ -2,7 +2,7 @@ import crypto = require('crypto');
 
 const DIPLOMA_WORK_SALT = process.env.DIPLOMA_WORK_SALT || '';
 
-export function encryptPassword(password: string): Promise<Error | string> {
+export function encryptPassword(password: string): Promise<string> {
     return new Promise((resolve, reject) => {
         crypto.pbkdf2(password, DIPLOMA_WORK_SALT, 10, 64, 'sha512', (error: Error | null, derivedKey: Buffer) => {
             error ? reject(error) : resolve(derivedKey.toString('hex'));
