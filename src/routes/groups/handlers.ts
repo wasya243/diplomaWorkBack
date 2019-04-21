0a949d9ba1e5c7906a72f3f3cc6cae94e61ce3c0
Vasyl Kharhenko <v.kharchenko@mozidev.com>
2019-04-21 13:59:05 +0300

    Fix getGroups handler to give groups by faculty relation.

--- src/routes/groups/handlers.ts
+++ src/routes/groups/handlers.ts
@@ -1,14 +1,31 @@
 import * as express from 'express';
 
 import { DatabaseManager } from '../../db/database-manager';
-import { Group } from '../../db/models';
+import { Group, User, Dispatcher } from '../../db/models';
 
 export async function getGroups(req: express.Request, res: express.Response, next: express.NextFunction) {
+    // @ts-ignore
+    const userId = req.userData.id;
     try {
         const connection = DatabaseManager.getConnection();
+
         const groupRepository = connection.getRepository(Group);
-        const allGroups = await groupRepository.find({});
-        res.send(allGroups);
+        const userRepository = connection.getRepository(User);
+        const dispatcherRepository = connection.getRepository(Dispatcher);
+
+        const user = await userRepository.findOne(userId, { relations: [ 'role' ] });
+
+        let groups = [];
+
+        if (user && user.role.name === 'dispatcher') {
+            const dispatcher = await dispatcherRepository.findOne(userId, { relations: [ 'faculty' ] });
+            groups = await groupRepository.find({ faculty: dispatcher && dispatcher.faculty });
+
+        } else {
+            groups = await groupRepository.find({});
+        }
+
+        res.send(groups);
     } catch (error) {
         next(error);
     }
